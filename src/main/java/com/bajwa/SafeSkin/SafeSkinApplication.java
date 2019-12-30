7d8ce9619c4ec27c7ffeedd2814efb6525eaeef7
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-12-30 16:21:57 -0500

    first commit

---
+++ src/main/java/com/bajwa/SafeSkin/SafeSkinApplication.java
@@ -0,0 +1,13 @@
+package com.bajwa.SafeSkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeSkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeSkinApplication.class, args);
+	}
+
+}
