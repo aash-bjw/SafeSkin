ea3cd6f2d0384867163fc7916ffed531f809e348
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2020-01-01 21:58:50 -0500

    some class shells

---
+++ src/main/java/com/bajwa/SafeSkin/controllers/SkinCareController.java
@@ -0,0 +1,7 @@
+package com.bajwa.SafeSkin.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SkinCareController {
+}
---
+++ src/main/java/com/bajwa/SafeSkin/services/SkinCareService.java
@@ -0,0 +1,9 @@
+package com.bajwa.SafeSkin.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SkinCareService {
+
+
+}
